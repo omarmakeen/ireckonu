803298ebb269096b6a6ad8c03dcd5d16fbd388cc
Makeen <omar.makeen2@vodafone.com>
2019-10-24 15:49:19 +0200

    ENHANCEMENT: add readme info

--- src/config/pages-config.ts
+++ src/config/pages-config.ts
@@ -1,18 +1,4 @@
 export const config = {
-    generalError: {
-        name: 'allgemeiner_fehler',
-        route: '/allgemeiner_fehler'
-    },
-    unauthorized: {
-        name: 'nicht_erlaubt',
-        route: '/nicht_erlaubt',
-        navigation: {
-            topLink: true,
-            mobileBtn: false,
-            navigationLink: '/meinvodafone/services/',
-            mobileBtnLabelKey: 'common.buttons.cancel.std-headline'
-        }
-    },
     profileSearch: {
         name: 'profile',
         route: '/profile',
