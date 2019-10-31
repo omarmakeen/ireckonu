323e512aee7af8f2bf9800c8d3751717c282cbfc
Makeen <omar.makeen2@vodafone.com>
2019-10-31 18:03:26 +0200

    DEVELOP appy keyup in search-bar

---
+++ src/app/shared/pipes/dynamic-pipe/dynamic-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DynamicPipePipe } from './dynamic-pipe.pipe';
+
+describe('DynamicPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DynamicPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
